d871243a93fed4f58d8584a3d592c7d30200031f
AndreasFagschlunger <andreas@fagschlunger.co.at>
2019-10-04 22:52:55 +0200

    Secure variables, gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,30 @@
+/*
+ * BSD 2-Clause License
+ * 
+ * Copyright (c) 2019, AndreasFagschlunger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 plugins {
     `java-library`
 }
@@ -11,10 +38,29 @@ group = "at.o2xfs"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    if((version as String).endsWith("-SNAPSHOT")) {
+        maven {
+            url = uri("https://repo.fagschlunger.co.at/libs-snapshot-local")
+        }
+    }
     maven {
-        url = uri("https://repo.fagschlunger.co.at/list/libs-snapshot-local")
+        url = uri("https://repo.fagschlunger.co.at/libs-release-local")
     }
     mavenCentral()
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allJava)
+}
+
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc.get().destinationDir)
 }
 
 dependencies {
@@ -24,6 +70,25 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = if((version as String).endsWith("-SNAPSHOT")) uri("https://repo.fagschlunger.co.at/libs-snapshot-local") else uri("https://repo.fagschlunger.co.at/libs-release-local")
+            credentials {
+                val maven_username: String? by project
+                val maven_password: String? by project
+                username = maven_username
+                password = maven_password
+            }
+        }
+    }
 }
