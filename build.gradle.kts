3dc518605f76ea2c5dc92b59b4ab50474dc5cfb2
AndreasFagschlunger <andreas@fagschlunger.co.at>
2019-10-04 22:58:54 +0200

    build.gradle.kts: plugin: maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@
  */
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 java {
@@ -49,6 +50,13 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    api("at.o2xfs:o2xfs-common:1.0-SNAPSHOT")
+    implementation("org.apache.commons:commons-lang3:3.9")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
 tasks.named<Test>("test") {
     useJUnitPlatform()
 }
@@ -61,13 +69,6 @@ tasks.register<Jar>("sourcesJar") {
 tasks.register<Jar>("javadocJar") {
     archiveClassifier.set("javadoc")
     from(tasks.javadoc.get().destinationDir)
-}
-
-dependencies {
-    api("at.o2xfs:o2xfs-common:1.0-SNAPSHOT")
-    implementation("org.apache.commons:commons-lang3:3.9")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
 
 publishing {
